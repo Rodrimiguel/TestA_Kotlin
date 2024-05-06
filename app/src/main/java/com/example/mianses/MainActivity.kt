99d422dce95050959552615b84dd121279aa2f97
RoderickMFoggia <rodrigomfoggia84@gmail.com>
2024-05-06 19:29:23 -0300

    Icon Added

--- app/src/main/java/com/example/mianses/MainActivity.kt
+++ app/src/main/java/com/example/mianses/MainActivity.kt
@@ -8,9 +8,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +24,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mianses.ui.theme.MiAnsesTheme
@@ -62,12 +69,28 @@ fun MiAnsesView(name: String, modifier: Modifier = Modifier) {
                 .padding(top = 10.dp)
                 .fillMaxWidth(1f)
         )
-        Button(onClick = {}, modifier.align(Alignment.CenterHorizontally).padding(10.dp)) {
+        Button(onClick = {},
+            modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp)) {
             Text(
                 text = "Ingresar",
                 modifier = modifier.padding(horizontal = 10.dp)
             )
         }
+        Text(
+            text = "Olvidé mi clave",
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        )
+        Divider(modifier.padding(vertical =  30.dp), thickness = 3.dp, color = Color.Black)
+        Text(
+            text = "Usá tu huella digital",
+            modifier = modifier.align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = modifier.height(20.dp)) // sorted by weight
+        Icon(imageVector = Icons.Rounded.AccountCircle, contentDescription = "Descripción del Icono",
+        modifier = modifier.align(Alignment.CenterHorizontally).size(80.dp)
+        )
     }
 }
 
